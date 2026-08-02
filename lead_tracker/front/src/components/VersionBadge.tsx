bf67731673689711bd4fb6b014cb2e0316cd469e
EL-MHAMDI Ilias <ilelmham@bouyguestelecom.fr>
2026-08-02 13:37:15 +0200

    badge

--- lead_tracker/front/src/components/VersionBadge.tsx
+++ lead_tracker/front/src/components/VersionBadge.tsx
@@ -3,5 +3,5 @@ import { useVersion } from '../api/version';
 export function VersionBadge() {
   const { data: version } = useVersion();
 
-  return <div className="fixed right-3 bottom-3 z-10 text-xs text-white/30 select-none">v{version ?? '...'}</div>;
+  return <div className="fixed right-3 bottom-3 z-[60] text-xs text-white/30 select-none">v{version ?? '...'}</div>;
 }
